beb93b43decdd17eaf4c6f103eff8cccf32fd015
ridakk <kadirgoktas@gmail.com>
2019-08-15 00:04:40 +0300

    fix: added favicon generator

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -9,6 +10,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin('./assets/images/boy-1294181_1280.png'),
     new CopyPlugin([
       { from: 'assets', to: 'assets' },
     ]),
