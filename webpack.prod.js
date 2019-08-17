4a5bc4a2261a1eb325424767ce841389cc0c9cd3
ridakk <kadirgoktas@gmail.com>
2019-08-17 22:51:59 +0300

    fix: added minification to production setup

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const common = require('./webpack.common.js');
 
@@ -8,4 +9,15 @@ module.exports = merge(common, {
   // plugins: [
   //   new FaviconsWebpackPlugin('./assets/images/<favicon>.png'),
   // ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
 });
