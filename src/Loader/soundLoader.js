d1b865b9472079225b322cab59ed9498b2340e93
ridakk <kadirgoktas@gmail.com>
2019-08-20 22:59:20 +0300

    fix: #14 added sound loader

--- src/Loader/soundLoader.js
+++ src/Loader/soundLoader.js
@@ -1,7 +1,14 @@
 import * as PIXI from 'pixi.js';
+import * as _ from 'lodash';
+import fileName from '../utils/fileName';
 
 const sharedLoader = PIXI.Loader.shared;
 
 export default function addSounds(sounds) {
+  _.forEach(sounds, (sound) => {
+    const name = fileName(sound);
 
+    console.log(`${name} - ${sound}`);
+    sharedLoader.add(name, `/assets/sounds/${sound}`);
+  });
 }
